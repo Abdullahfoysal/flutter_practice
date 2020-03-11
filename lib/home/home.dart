ae206fa5fbbaef6572ce35de1458069e43ed2ec5
Abdullahfoysal <abdullah32@student.sust.edu>
2020-03-11 13:55:20 +0600

    ui design:stack,container,row,column,scaffold

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -7,17 +7,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _bottomNavIndex=0;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       bottomNavigationBar: new BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         fixedColor: Colors.green,
+
+        currentIndex: _bottomNavIndex,
+        onTap: (int index){
+          setState(() {
+            _bottomNavIndex =index;
+          });
+        },
+
         items: [
+          new BottomNavigationBarItem(
+            title: new Text('Home'),
+            icon: new Icon(Icons.home),
+          ),
           new BottomNavigationBarItem(
             title: new Text(''),
             icon: new Icon(Icons.home),
-          )
+          ),
+          new BottomNavigationBarItem(
+            title: new Text(''),
+            icon: new Icon(Icons.home),
+          ),
+          new BottomNavigationBarItem(
+            title: new Text(''),
+            icon: new Icon(Icons.home),
+          ),
         ],
 
       ),
